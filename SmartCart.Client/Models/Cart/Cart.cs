28736f65832fd0004d070a9a26f9d266ad6f41d4
Daniil <91062667+trnvdn@users.noreply.github.com>
2024-05-21 13:59:26 +0300

    Implementing models

---
+++ SmartCart.Client/Models/Cart/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartCart.Client.Models.Cart
+{
+    public class Cart
+    {
+        public Guid CartID { get; set; }
+        public string CartName { get; set; }
+        public string CartOwnerID { get; set; }
+    }
+}
