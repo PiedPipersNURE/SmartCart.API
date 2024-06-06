e3ca0756a5566f2dee12e96fd6629ffe6488ee3c
Daniil <91062667+trnvdn@users.noreply.github.com>
2024-06-06 11:40:24 +0300

    Remove Client project

--- SmartCart.Client/Models/CartDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SmartCart.Client.Models
-{
-    public class CartDto
-    {
-        public Guid CartID { get; set; }
-        public string CartName { get; set; }
-        public Guid CartOwnerID { get; set; }
-    }
-}
--- SmartCart.Client/Models/CartMemberDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SmartCart.Client.Models
-{
-    public class CartMemberDto
-    {
-        public Guid CartMemberID { get; set; }
-        public Guid CartID { get; set; }
-        public Guid MemberID { get; set; }
-    }
-}
